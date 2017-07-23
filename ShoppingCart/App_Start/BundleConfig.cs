cd755aa48f65ccb4ce63d85e8c6c2365958d9e44
caoxiaosheng <836900367@qq.com>
2017-07-23 22:56:02 +0800

    Update packages Create Bundle

--- ShoppingCart/App_Start/BundleConfig.cs
+++ ShoppingCart/App_Start/BundleConfig.cs
@@ -8,21 +8,15 @@ namespace ShoppingCart
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
+            bundles.Add(new ScriptBundle("~/bundles/shoppingCart").Include("~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery.validate*", "~/Scripts/jquery-ui.js", "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js", "~/Scripts/knockout-{version}.js"));
+            
             // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
